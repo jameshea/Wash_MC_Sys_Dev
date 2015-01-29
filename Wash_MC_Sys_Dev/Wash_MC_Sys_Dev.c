58d9c34940271ab4419b4c07762e07a0f7543e57
jameshea <jamesgheath1994@gmail.com>
2015-01-29 15:29:21 -0500

    Lab 4 and 5 for ECET279

---
+++ Wash_MC_Sys_Dev/Wash_MC_Sys_Dev.c
@@ -0,0 +1,17 @@
+/*
+ * Wash_MC_Sys_Dev.c
+ *
+ * Created: 1/29/2015 3:23:28 PM
+ *  Author: User
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
